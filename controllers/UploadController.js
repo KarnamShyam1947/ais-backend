bba2afcb6b67dfb97ccfeaeda7fefe06052d32eb
KarnamShyam1947 <karnamshyamgithub@gmail.com>
2025-08-03 08:40:47 +0530

    feature : r2 upload added

--- controllers/UploadController.js
+++ controllers/UploadController.js
@@ -3,7 +3,7 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("./../config/CloudinaryConfig");
 const { extractPublicIdFromUrl } = require("./../lib/CloudniaryUtils");
 const { v4 } = require('uuid');
-const s3Client = require("../config/S3Client");
+const { s3Client, r2Client } = require("../config/S3Client");
 const { PutObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 
 const multerCloudinaryStorage = new CloudinaryStorage({
@@ -96,9 +96,55 @@ const deleteS3 = async (req, res) => {
     }
 }
 
+const uploadR2 = async (req, res) => {
+    if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded.' });
+    }
+
+    const folderName = req.body.folderName || 'uploads';
+    const fileKey = `${folderName}/${v4()}-${req.file.originalname}`;
+    const mimeType = req.file.mimetype; 
+
+    const params = {
+        Bucket: process.env.R2_BUCKET_NAME,
+        Key: fileKey,
+        ContentType: mimeType, 
+        Body: req.file.buffer
+    };
+
+    try {
+        await r2Client.send(new PutObjectCommand(params));
+        const fileUrl = `${process.env.R2_PUBLIC_URL}/${fileKey}`;
+        res.status(200).json({ message: 'File uploaded successfully', url: fileUrl });
+    } catch (err) {
+        res.status(500).send({error: 'Error uploading file: ' + err.message});
+    }
+}
+
+const deleteR2 = async (req, res) => {
+    const { fileKey } = req.body;
+
+    if (!fileKey) {
+        return res.status(400).json({error: 'File key is required.'});
+    }
+
+    try {
+        const params = {
+            Bucket: process.env.R2_BUCKET_NAME,
+            Key: fileKey,
+        };
+        await r2Client.send(new DeleteObjectCommand(params));
+        res.status(200).json({message: 'File deleted successfully'});
+    } catch (err) {
+        res.status(500).json({error: 'Error deleting file: ' + err.message});
+    }
+}
+
 module.exports = {
     uploadS3,
     deleteS3,
+    uploadR2,
+    deleteR2,
     uploadMemory,
     uploadCloudinary,
     uploadFileCloudinary,
